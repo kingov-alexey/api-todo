f37c6cc7a52d2fc454dca547aa1950cd9ba65cf6
kingov.alexey <kingov_alexey@mail.ru>
2020-07-16 00:47:39 +0600

    Init

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,23 +7,18 @@ use App\Task;
 
 class TaskController extends Controller
 {
-
-    //показать все
     public function index(){
         return Task::all();
     }
 
-    //показать определенное
     public function show($id){
         return Task::find($id);
     }
 
-    //создать задачу
     public function store(Request $request){
         return Task::create($request->all());
     }
 
-    //обновить определенную задачу
     public function update(Request $request, $id){
         $task = Task::findOrFail($id);
         $task->update($request->all());
@@ -32,7 +27,6 @@ class TaskController extends Controller
         return $task;
     }
 
-    //удалить определенную задачу
     public function delete(Request $request, $id){
         $task = Task::findOrFail($id);
         $task->delete();
